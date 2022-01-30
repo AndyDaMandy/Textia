a9837d3af41c176cd9c535356f9bc24a2cbdbff8
Andy M <maldonado.andy.m@gmail.com>
2022-01-30 00:07:55 +0000

    Added in a ton of stuff, setting up menus, etc.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let ando = {
   name: "Ando",
+  level: 1,
   hp: 10,
   mp: 5,
   pAtk: 5,
@@ -11,6 +12,7 @@ let ando = {
 };
 let marie = {
   name: "Marie",
+  level: 1,
   hp: 8,
   mp: 10,
   pAtk: 2,
@@ -24,6 +26,7 @@ let marie = {
 //enemies go here
 let iceman = {
   name: "Ice Man",
+  level: 1,
   hp: 8,
   mp: 10,
   pAtk: 2,
@@ -34,9 +37,6 @@ let iceman = {
   type: "enemy"
 };
 
-//========================
-// event listeners go here
-//===========================
 let inventory = [];
 let weaponsOwned = [];
 let armorOwned = [];
@@ -44,23 +44,26 @@ let accesoriesOwned = [];
 let keyItems = [];
 // function save (){};
 // Query Selectors
-
+//======================================
+//Menu items
 function showStats (x) {
-const element = document.getElementById("stats-menu");
-let stats = document.getElementById("party-status")
-let statsLog = document.getElementById("menu-items");  
-//checks if this has been run before and clears the content ahead of it.
-if (statsLog != ""){
+  const element = document.getElementById("stats-menu");
+  let stats = document.getElementById("party-status")
+  let statsLog = document.getElementById("menu-items");  
+  //checks if this has been run before and clears the content ahead of it.
+  if (statsLog != ""){
   
   statsLog.innerHTML = "";
-}
-stats.innerHTML ="Your current party: " + x[0].name + ", " + x[1].name;
-document.getElementById("line-1").innerHTML = "Here are their stats:";
+  }
+  stats.innerHTML ="Your current party: " + x[0].name + ", " + x[1].name;
+  document.getElementById("line-1").innerHTML = "Here are their stats:";
   for (let i = 0; i < currentParty.length; i++){
     let li = document.createElement("li");
     li.innerHTML = "Name: " + x[i].name;
       statsLog.appendChild(li);
-      li = document.createElement("li")
+      li = document.createElement("li");
+    li.innerHTML = "Level: " + x[i].level;
+      statsLog.appendChild(li);
     li.innerHTML = "HP: " + x[i].hp;
       statsLog.appendChild(li);
       li = document.createElement("li");
@@ -83,12 +86,10 @@ document.getElementById("line-1").innerHTML = "Here are their stats:";
      statsLog.appendChild(li);
   }
 };
-
-
-
 //=======================================
 //======================================
 //Menu functionality
+
 let menu = document.getElementById("open-menu");
 function openMenu(){
   document.getElementById("save-button").hidden = false;
@@ -105,11 +106,32 @@ function closeMenu() {
     document.getElementById("menu-items").innerHTML = "";
   document.getElementById("close-menu").hidden = true;
 }
+/*
+The game flow will be simple. The game will go through "Areas", each one being a div. Each Div will be a town/level or pass through. By toggling the hidden property in html we can hide the divs as necessary. The progression will be linear, at least for now....
+A gamestate value will be assigned to each area/location, thus allowing the travel function to take you through each level. Battles will need to be manually accessed I think...
+*/
 //=====================================
+// Area 1
+const townOne = document.getElementById("town-1");
+
+//=====================================
+
 
 let enemyParty = [];
 let currentParty = [];
-//======================================
+
 // all testing goes below
 currentParty = [ando, marie];
 enemyParty = [iceman.name, iceman.name, iceman.name];
+//======================================
+// Start Game , must go below everything else
+//======================================
+const begin = document.getElementById("begin");
+const start = document.getElementById("start-game");
+let gameState = 0;
+function startGame(){
+  menu.hidden = false;
+  townOne.hidden = false;
+  begin.hidden = true;
+  gameState = 1;
+}

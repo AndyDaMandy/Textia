53fa1456708b292f4777aa31388fdfe9d2f53956
AndyDaMandy <maldonady.andy.m@gmail.com>
2022-03-28 09:25:54 -0400

    removed testing features

--- globalvars.js
+++ globalvars.js
@@ -34,8 +34,7 @@ ari.weapon = ironDaggers;
 ari.skills.push(steal);
 ari.skills.push(quickSlashes);
 siege.weapon = ironSpear;
-//ari.skills.push(fire);
-weaponsOwned.push(excalibur);
+//weaponsOwned.push(excalibur);
 
 function toggleChests(){
     opened.hidden = true;
